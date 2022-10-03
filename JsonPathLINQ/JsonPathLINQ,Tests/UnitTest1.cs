ce6c41e307285305705ef70970898e88bf2823d3
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-10-03 00:20:52 -0700

    feat: initial filter support

--- JsonPathLINQ/JsonPathLINQ,Tests/UnitTest1.cs
+++ JsonPathLINQ/JsonPathLINQ,Tests/UnitTest1.cs
@@ -1,4 +1,8 @@
+using ExpressionTreeToString;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.Linq.Expressions;
 
 namespace JsonPathLINQ_Tests
 {
@@ -7,29 +11,62 @@ namespace JsonPathLINQ_Tests
         private class TestObject
         {
             public string stringValue { get; set; } = "TestString";
+
             public int intValue { get; set; } = 7;
 
-            public TestObject2 subClass { get; set; } = new TestObject2();
+            public bool boolValue { get; set; }
 
-            public List<TestObject2> subClassList { get; set; } = new List<TestObject2>()
+            public decimal decimalValue { get; set; } = 18.4M;
+
+            public double doubleValue { get; set; } = 12.23;
+
+            public TestObject2 subClass { get; set; } = new ();
+
+            public List<TestObject2> subClassList { get; set; } = new ()
             {
-                new TestObject2() { subStringValue = "1" },
-                new TestObject2() { subStringValue = "2" },
-                new TestObject2() { subStringValue = "3" },
+                new TestObject2() { Type = "1", Status = "Ready", Nested = new TestObject2.TestObject3(){ Name = "Nested1" } },
+                new TestObject2() { Type = "2", Status = "Terminated", Nested = new TestObject2.TestObject3(){ Name = "Nested2" } },
+                new TestObject2() { Type = "3", Status = "Starting", Nested = new TestObject2.TestObject3(){ Name = "Nested3" } },
             };
 
             public class TestObject2
             {
-                public string subStringValue { get; set; } = "SubTestString";
+                public string Type { get; set; } = "Type1";
+                public string Status { get; set; } = "Status1";
+
+                public TestObject3 Nested { get; set; } = new();
+
+                public class TestObject3
+                {
+                    public string Name { get; set; } = "Test3";
+                }
             }
         }
 
-        [Theory]
-        [InlineData(".stringValue", "TestString")]
-        //[InlineData(".intValue", 7)]
-        [InlineData(".subClass.subStringValue", "SubTestString")]
-        public void Test1(string jsonPath, object value)
+        public static IEnumerable<object[]> GetTests()
         {
+            return new List<object[]>
+            {
+                new object[] { ".stringValue", "TestString" },
+                new object[] { ".intValue", 7 },
+                new object[] { ".boolValue", false },
+                new object[] { ".decimalValue", 18.4 },
+                new object[] { ".doubleValue", 12.23 },
+                new object[] { ".subClass.Type", "Type1" },
+
+                new object[] { ".subClassList[?(@.Type==\"1\")].Status", "Ready"},
+                new object[] { ".subClassList[?(@.Nested.Name==\"Nested3\")].Status", "Starting"},
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTests))]
+        public void Tests(string jsonPath, object value)
+        {
+            //Expression<Func<TestObject, object>> test = x => x.subClassList.Where(y => y.Type == "1");
+            //var one = test.ToString("C#");
+            //var two = test.ToString("Object notation", "C#");
+
             var expression = JsonPathLINQ.JsonPathLINQ.GetExpression<TestObject>(jsonPath);
 
             expression.Compile().Invoke(new TestObject()).Should().Be(value);
--- JsonPathLINQ/JsonPathLINQ/JsonPathLINQ.cs
+++ JsonPathLINQ/JsonPathLINQ/JsonPathLINQ.cs
@@ -1,6 +1,8 @@
 ﻿using JsonPathExpressions;
 using JsonPathExpressions.Elements;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace JsonPathLINQ
@@ -12,6 +14,7 @@ namespace JsonPathLINQ
             var jsonPathExpression = new JsonPathExpression(jsonPath).GetNormalized();
 
             var param = Expression.Parameter(typeof(T), "x");
+
             Expression body = param;
 
             foreach (var element in jsonPathExpression.Elements)
@@ -40,13 +43,72 @@ namespace JsonPathLINQ
                     case JsonPathElementType.Expression:
                         break;
                     case JsonPathElementType.FilterExpression:
+                        var param2 = Expression.Parameter(body.Type.GenericTypeArguments[0], "y");
+
+                        var filter = ProcessFilterExpression(param2, ((JsonPathFilterExpressionElement)element).Expression);
+
+                        var newFunc = typeof(Func<,>).MakeGenericType(body.Type.GenericTypeArguments[0], typeof(bool));
+
+                        var filterFunc = Expression.Lambda(newFunc, filter, param2);
+
+                        body = Expression.Call(typeof(Enumerable), nameof(Enumerable.FirstOrDefault), new[] { body.Type.GenericTypeArguments[0] }, body, filterFunc);
                         break;
                     default:
                         break;
                 }
             }
 
-            return Expression.Lambda<Func<T, object>>(body, param);
+            Expression conversion = Expression.Convert(body, typeof(object));
+            return Expression.Lambda<Func<T, object>>(conversion, param);
+        }
+
+        static private Expression ProcessFilterExpression(ParameterExpression param, string jsonExpression)
+        {
+            //@.type=="Ready"
+            //@	the current object/element
+            //* wildcard.All objects / elements regardless their names.
+            var isWildcard = jsonExpression[..1] == "*";
+
+            var isCurrent = jsonExpression[..1] == "@";
+
+            var left = jsonExpression[1..];
+            var right = string.Empty;
+
+            // ==	left is equal to right (note that 1 is not equal to '1').
+            // !=	left is not equal to right.
+            // <	left is less than right.
+            // <=	left is less or equal to right.
+            // >	left is greater than right.
+            // >=	left is greater than or equal to right.
+
+            var op = string.Empty;
+
+            foreach (var opStr in new List<string>(){ "==", "!=", "<", "<=", ">", ">=" })
+            {
+                if (left.IndexOf(opStr) > -1)
+                {
+                    op = opStr;
+                    right = left.Substring(left.IndexOf(opStr) + 2);
+                    left = left.Substring(0, left.IndexOf(opStr))[1..];
+
+                    return Expression.Equal(param.GetNestedProperty(left), Expression.Constant(right.Trim('"')));
+                };
+            }
+
+            return null;
+        }
+
+        internal static MemberExpression GetNestedProperty(this Expression param, string property)
+        {
+            var propNames = property.Split('.');
+            var propExpr = Expression.Property(param, propNames[0]);
+
+            for (int i = 1; i < propNames.Length; i++)
+            {
+                propExpr = Expression.Property(propExpr, propNames[i]);
+            }
+
+            return propExpr;
         }
     }
 }
